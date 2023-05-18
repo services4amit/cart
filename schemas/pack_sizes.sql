fb347543277b0fd208b729c1870ec0ca45eee159
services4amit <44164368+services4amit@users.noreply.github.com>
2023-05-19 01:27:09 +0530

    Update pack_sizes.sql

--- schemas/pack_sizes.sql
+++ schemas/pack_sizes.sql
@@ -1,10 +1,13 @@
 CREATE TABLE pack_sizes (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  product_id INT,
-    `product_name` varchar(255) NOT NULL,
-      `mrp` decimal(10,2) NOT NULL,
-        `offered_price` decimal(10,2) NOT NULL,
-  no_of_packs INT,
-  pack_size INT,
-  description VARCHAR(255) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    product_name VARCHAR(255) NOT NULL,
+    mrp DECIMAL(10 , 2 ) NOT NULL,
+    offered_price DECIMAL(10 , 2 ) NOT NULL,
+    no_of_packs INT,
+    pack_size INT,
+    description VARCHAR(255) NOT NULL,
+    net_weight INT AS (pack_size * no_of_packs),
+    total_price DECIMAL(10 , 2 ) AS (net_weight * offered_price),
+   discount DECIMAL(10 , 2 ) AS(((obj.mrp - obj.offered_price) / obj.mrp) * 100)
 );
