b8fffbe83af6404ea340efd90cfcbde17259b732
srichandan <srichandan99@gmail.com>
2023-05-21 09:45:31 +0530

    changed post to get and aligned res

--- routes/order.js
+++ routes/order.js
@@ -11,6 +11,6 @@ router.post("/", order);
 router.get("/past/:id", getOrderByCustomerId);
 router.get("/active/:id", getOrderByCustomerId);
 
-router.post("/stockavalabilitybyproduct", getStockAvailabailityByProduct);
+router.get("/stockavalabilitybyproduct", getStockAvailabailityByProduct);
 
 module.exports = router;
