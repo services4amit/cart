707f430b1dc94b434ebcb65eeb5ebb8de14aec47
services4amit <44164368+services4amit@users.noreply.github.com>
2023-05-23 20:10:40 +0530

    Update cart.sql

--- schemas/cart.sql
+++ schemas/cart.sql
@@ -1,11 +1,13 @@
-CREATE TABLE cart (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    product_id INT,
-    pack_id INT,
-    product_quantity INT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    updated_by VARCHAR(255),
-    created_by VARCHAR(255)
+CREATE TABLE `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `pack_id` int DEFAULT NULL,
+  `product_quantity` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`, `product_id`),
+  UNIQUE KEY `id` (`id`)
 );
